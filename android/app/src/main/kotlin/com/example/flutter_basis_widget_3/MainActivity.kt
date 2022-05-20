0b73b864821bde0ccc5c105028e579eca1d49c9e
Taha Turan <tahaaturann16@gmail.com>
2022-05-20 08:32:32 +0300

    Flutter Temel Widgetlar

---
+++ android/app/src/main/kotlin/com/example/flutter_basis_widget_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_basis_widget_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/card_listtile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardveListtileKullanimi extends StatelessWidget {
+  const CardveListtileKullanimi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Card Ve Listtile Kullanimi"),
+      ),
+      body: Container(),
+    );
+  }
+}
