dd50ade9392f18859be7ecdc5ac69f8d40f2933d
Taha Turan <tahaaturann16@gmail.com>
2022-05-26 10:58:32 +0300

    WillPopScope Kullanimi

--- lib/views/redpage.dart
+++ lib/views/redpage.dart
@@ -8,26 +8,34 @@ class RedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Red Page'),
-        backgroundColor: Colors.red,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            const Text("Red Page"),
-            ElevatedButton(
-              onPressed: () {
-                rasgeleSayi = Random().nextInt(100);
-                debugPrint("Uretilen Sayi $rasgeleSayi");
-                Navigator.pop(context, rasgeleSayi);
-              },
-              child: const Text(
-                "AnaSayfa ya veri Gonder",
+    return WillPopScope(
+      onWillPop: () {
+        debugPrint("onWillPop Calisti");
+        rasgeleSayi = Random().nextInt(100);
+        Navigator.pop(context, rasgeleSayi);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Red Page'),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Text("Red Page"),
+              ElevatedButton(
+                onPressed: () {
+                  rasgeleSayi = Random().nextInt(100);
+                  debugPrint("Uretilen Sayi $rasgeleSayi");
+                  Navigator.pop(context, rasgeleSayi);
+                },
+                child: const Text(
+                  "AnaSayfa ya veri Gonder",
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
