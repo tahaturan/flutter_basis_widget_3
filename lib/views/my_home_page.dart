be20a3ae27947233474020bc2e232305ae9c0b77
Taha Turan <tahaaturann16@gmail.com>
2022-05-26 10:54:38 +0300

    geri donuste veri almak

--- lib/views/my_home_page.dart
+++ lib/views/my_home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_basis_widget_3/views/card_listtile.dart';
 import 'package:flutter_basis_widget_3/views/custom_scroll_ve_slivers.dart';
 import 'package:flutter_basis_widget_3/views/gridview_kullanimi.dart';
 import 'package:flutter_basis_widget_3/views/listview_kullanimi.dart';
+import 'package:flutter_basis_widget_3/views/redpage.dart';
 import 'package:flutter_basis_widget_3/widgets/sayfa_gecis_button.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -17,37 +18,55 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("AnaSayfa"),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              SayfaGecisButton(
-                  isim: "Card & Listtile Kullanimi",
-                  sayfa: const CardveListtileKullanimi()),
-              SayfaGecisButton(
-                  isim: "ListView Kullanimi", sayfa: ListViewKullanimi()),
-              SayfaGecisButton(
-                  isim: "GridView Kullanimi", sayfa: const GridviewKullanimi()),
-              SayfaGecisButton(
-                  isim: "Custom Sctoll & Slivers",
-                  sayfa: const CustomScrollandSlivers()),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    CupertinoPageRoute(
-                      builder: (context) => const CustomScrollandSlivers(),
-                    ),
-                  );
-                },
-                child: const Text(
-                  "IOS icin Gecis Butonu",
-                ),
+      appBar: AppBar(
+        title: const Text("AnaSayfa"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SayfaGecisButton(
+                isim: "Card & Listtile Kullanimi",
+                sayfa: const CardveListtileKullanimi()),
+            SayfaGecisButton(
+                isim: "ListView Kullanimi", sayfa: ListViewKullanimi()),
+            SayfaGecisButton(
+                isim: "GridView Kullanimi", sayfa: const GridviewKullanimi()),
+            SayfaGecisButton(
+                isim: "Custom Sctoll & Slivers",
+                sayfa: const CustomScrollandSlivers()),
+            ElevatedButton(
+              onPressed: () async {
+                int? gelenSayi = await Navigator.push<int>(
+                  context,
+                  CupertinoPageRoute(
+                    builder: (context) => RedPage(),
+                  ),
+                );
+                debugPrint("Anasayfaya Gelen Sayi $gelenSayi");
+              },
+              child: const Text(
+                "IOS icin Gecis Butonu",
               ),
-            ],
-          ),
-        ));
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push<int>(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RedPage(),
+                  ),
+                ).then(
+                  (int? value) =>
+                      debugPrint("Anasayfaya Then ile gelen veri $value"),
+                );
+              },
+              child: const Text(
+                "Then Kullanarak Veriyi alma",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/views/redpage.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class RedPage extends StatelessWidget {
+  RedPage({Key? key}) : super(key: key);
+  int rasgeleSayi = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Red Page'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("Red Page"),
+            ElevatedButton(
+              onPressed: () {
+                rasgeleSayi = Random().nextInt(100);
+                debugPrint("Uretilen Sayi $rasgeleSayi");
+                Navigator.pop(context, rasgeleSayi);
+              },
+              child: const Text(
+                "AnaSayfa ya veri Gonder",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
