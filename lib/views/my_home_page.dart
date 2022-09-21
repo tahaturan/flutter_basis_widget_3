760499c1baa813d8075c60e495ab675a244e66da
Taha Turan <tahaaturann16@gmail.com>
2022-09-21 15:54:20 +0300

    Bildirim Olsuturma Islemleri

--- lib/views/my_home_page.dart
+++ lib/views/my_home_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_basis_widget_3/views/gridview_kullanimi.dart';
 import 'package:flutter_basis_widget_3/views/listview_kullanimi.dart';
 import 'package:flutter_basis_widget_3/views/redpage.dart';
 import 'package:flutter_basis_widget_3/widgets/sayfa_gecis_button.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -15,6 +16,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var flp = FlutterLocalNotificationsPlugin();
+  //*bunu kullanarak bildirim gostebilecegiz
+
+  Future<void> kurulum() async {
+    var androidAyari =
+        const AndroidInitializationSettings("@mipmap/ic_launcher");
+    var iosAyari = DarwinInitializationSettings(
+      onDidReceiveLocalNotification: (id, title, body, payload) {
+        id = 1;
+        title = "deneme";
+        body = "denemee";
+        payload = "payload";
+      },
+    );
+    var kurulumAyari =
+        InitializationSettings(android: androidAyari, iOS: iosAyari);
+
+    await flp.initialize(
+      kurulumAyari,
+      onDidReceiveNotificationResponse: (details) => bildirimSecilme,
+    );
+  }
+
+  Future<void> bildirimSecilme(String payload) async {
+    if (payload != null) {
+      debugPrint("Bildirim Secildi = $payload");
+    }
+  }
+
+  Future<void> bildirimGoster() async {
+    var androidBildirimDetay = const AndroidNotificationDetails(
+        "kanal id", "kanal baslik",
+        channelDescription: "kanal aciklama",
+        priority: Priority.high,
+        importance: Importance.max);
+    var bildirimDetayi = NotificationDetails(android: androidBildirimDetay);
+    await flp.show(0, "Baslik", "Icerik", bildirimDetayi,
+        payload: "payload Icerik");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    kurulum();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,6 +125,21 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: const Text(
                 "Liste Olustur",
+              ),
+            ),
+            const Divider(color: Colors.black),
+            ElevatedButton(
+              onPressed: () {
+                bildirimGoster();
+              },
+              child: const Text(
+                "Bildirim Olustur",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                "Bildirim Olustur (Gaecikmeli)",
               ),
             ),
           ],
